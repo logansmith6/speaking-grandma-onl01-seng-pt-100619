26bf91eb7695dcdc092912a6d5401f22ed64cb9e
Logan Smith <logsmith06@gmail.com>
2019-10-08 18:23:18 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -22,14 +22,14 @@
 #
 
 def speak_to_grandma(string = "HI GRANDMA")
-  if string == string.upcase
+  if string == "I LOVE YOU GRANDMA!"
+    puts "I LOVE YOU TOO PUMPKIN!"
+    return "I LOVE YOU TOO PUMPKIN!"
+  elsif string == string.upcase
     puts "NO, NOT SINCE 1938!"
     return "NO, NOT SINCE 1938!"
   elsif string != string.upcase
     puts "HUH?! SPEAK UP, SONNY!"
     return "HUH?! SPEAK UP, SONNY!"
-  elsif string == "I LOVE YOU GRANDMA!"
-    puts "I LOVE YOU TOO PUMPKIN!"
-    return "I LOVE YOU TOO PUMPKIN!"
   end
 end
